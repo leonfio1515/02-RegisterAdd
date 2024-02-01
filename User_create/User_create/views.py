55949493e211e07f3518d9706189c0efadb854cc
leonfio1515 <106787709+leonfio1515@users.noreply.github.com>
2024-02-01 20:54:20 -0300

    add Readme

--- User_create/User_create/views.py
+++ User_create/User_create/views.py
@@ -100,7 +100,7 @@ class ClientRegister(LoginRequiredMixin, CreateView):
         client.save()
         send_mail_client(client, usuario)
 
-        return redirect('client_register')
+        return redirect('client_list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
